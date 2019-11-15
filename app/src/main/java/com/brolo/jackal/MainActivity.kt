b649ea375610e8c8a58627ce06c168bb174fd267
Harrison Klucher <hklucher@gmail.com>
2019-11-15 15:54:11 -0500

    Initial commit

---
+++ app/src/main/java/com/brolo/jackal/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.brolo.jackal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.brolo.jackal.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow(
+
+                    )
+        }
+    }
+
+}

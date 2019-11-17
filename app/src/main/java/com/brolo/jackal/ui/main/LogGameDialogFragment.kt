84087b11f448177560b2105c3885e8fbc2ee16e4
Harrison Klucher <hklucher@gmail.com>
2019-11-17 12:06:03 -0500

    Delete MainFragment and move behavior up to MainActivity, implement interface to create a game from dialog fragment

--- app/src/main/java/com/brolo/jackal/ui/main/LogGameDialogFragment.kt
+++ app/src/main/java/com/brolo/jackal/ui/main/LogGameDialogFragment.kt
@@ -2,12 +2,23 @@ package com.brolo.jackal.ui.main
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.brolo.jackal.R
+import com.brolo.jackal.mdoel.Game
+import java.lang.ClassCastException
 import java.lang.IllegalStateException
 
 class LogGameDialogFragment : DialogFragment() {
+    internal lateinit var listener: LogGameDialogFragmentListener
+
+    val game = Game(0, "attack", null)
+
+    interface LogGameDialogFragmentListener {
+        fun onGameCreated(game: Game)
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
@@ -15,7 +26,7 @@ class LogGameDialogFragment : DialogFragment() {
 
             builder.setView(inflater.inflate(R.layout.dialog_log_game, null))
                 .setPositiveButton(R.string.create, { dialog, id ->
-
+                    listener.onGameCreated(game)
                 })
                 .setNegativeButton(R.string.cancel, { dialog, id ->
                     getDialog()?.cancel()
@@ -23,5 +34,15 @@ class LogGameDialogFragment : DialogFragment() {
 
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        try {
+            listener = context as LogGameDialogFragmentListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException("$context must implement LogGameDialogFragmentListener")
+        }
     }
 }

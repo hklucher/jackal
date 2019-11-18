18b144882bc39136555081776b1e48a4937d2725
Harrison Klucher <hklucher@gmail.com>
2019-11-17 20:15:22 -0500

    Update chart view from shared viewmodel

--- app/src/main/java/com/brolo/jackal/ui/main/PieChartFragment.kt
+++ app/src/main/java/com/brolo/jackal/ui/main/PieChartFragment.kt
@@ -1,18 +1,25 @@
 package com.brolo.jackal.ui.main
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.brolo.jackal.R
+import com.brolo.jackal.model.Game
+import com.brolo.jackal.viewmodel.GamesViewModel
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import kotlinx.android.synthetic.main.fragment_pie_chart.*
 
 class PieChartFragment : Fragment() {
+
+    private lateinit var viewModel: GamesViewModel
 
     companion object {
         fun newInstance(): PieChartFragment {
@@ -28,26 +35,50 @@ class PieChartFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_pie_chart, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
 
-        setupChart()
+        observeGamesViewModel()
+    }
+
+    private fun observeGamesViewModel() {
+        val gameObserver = Observer<List<Game>> {
+            setupChart()
+        }
+
+        activity?.let {
+            viewModel = ViewModelProviders.of(it).get(GamesViewModel::class.java)
+            viewModel.allGames.observe(it, gameObserver)
+        }
     }
 
     private fun setupChart() {
         val entries = mutableListOf<PieEntry>()
 
-        entries.add(PieEntry(40.0f, "Defense"))
-        entries.add(PieEntry(60.0f, "Attack"))
+        val allGames = viewModel.allGames.value
 
-        val dataSet = PieDataSet(entries, "")
-        dataSet.setColors(Color.parseColor("#ff6f00"), Color.parseColor("#1976d2"))
-        dataSet.setDrawValues(false)
+        allGames?.let {
+            val defenseGames = it.filter { game -> game.startingTeam == Game.TeamDefense }
+            val attackGames = it.filter { game -> game.startingTeam == Game.TeamAttack }
+            val defPercentage = (defenseGames.size.toFloat() / allGames.size.toFloat()) * 100
+            val atkPercentage = (attackGames.size.toFloat() / allGames.size.toFloat()) * 100
 
-        val pieChartData = PieData(dataSet)
+            entries.add(PieEntry(defPercentage, "Defense"))
+            entries.add(PieEntry(atkPercentage, "Attack"))
 
-        pie_chart.holeRadius = 0.0f
-        pie_chart.transparentCircleRadius = 0.0f
-        pie_chart.data = pieChartData
+            val dataSet = PieDataSet(entries, "")
+            val pieChartData = PieData(dataSet)
+
+            dataSet.setColors(
+                Color.parseColor("#ff6f00"),
+                Color.parseColor("#1976d2")
+            )
+            dataSet.setDrawValues(false)
+
+            pie_chart.holeRadius = 0.0f
+            pie_chart.transparentCircleRadius = 0.0f
+            pie_chart.data = pieChartData
+            pie_chart.invalidate()
+        }
     }
 }

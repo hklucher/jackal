fe747af1b664484d6c280183bf26cdcf55f46ef2
Harrison Klucher <hklucher@gmail.com>
2020-02-03 19:58:27 -0500

    Add user model

---
+++ app/src/main/java/com/brolo/jackal/model/User.kt
@@ -0,0 +1,3 @@
+package com.brolo.jackal.model
+
+data class User(val id: Int, val email: String)
--- app/src/main/java/com/brolo/jackal/ui/main/LoginActivity.kt
+++ app/src/main/java/com/brolo/jackal/ui/main/LoginActivity.kt
@@ -1,15 +1,27 @@
 package com.brolo.jackal.ui.main
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatAutoCompleteTextView
 import com.brolo.jackal.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         supportActionBar?.hide()
+        setupSubmitButton()
+    }
+
+    private fun setupSubmitButton() {
+        btn_submit.setOnClickListener {
+            btn_submit.text = getString(R.string.submitting)
+            btn_submit.isEnabled = false
+            btn_submit.setBackgroundColor(resources.getColor(R.color.colorGreyBackground))
+
+            val email = login_field_email_input.text.toString()
+            val password = login_field_password_input.text.toString()
+        }
     }
 }

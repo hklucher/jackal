1e8ba8ddbc42483a5f2fba1eed5b873f61251592
Harrison Klucher <hklucher@gmail.com>
2020-04-26 17:54:30 -0400

    Setup settings UI

--- app/src/main/java/com/brolo/jackal/ui/main/SettingsFragment.kt
+++ app/src/main/java/com/brolo/jackal/ui/main/SettingsFragment.kt
@@ -1,18 +1,11 @@
 package com.brolo.jackal.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import com.brolo.jackal.R
 
-class SettingsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.user_preferences, rootKey)
     }
 }
